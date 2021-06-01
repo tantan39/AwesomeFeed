760b0be01f05634fab266e4c8f76d4b49fef0f2b
Tan Tran <tantan39>
2021-06-01 09:39:31 -0400

    Does not delete cache upon LocalFeedLoader creation

---
+++ AwesomeFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,31 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  AwesomeFeedTests
+//
+//  Created by Tan Tan on 6/1/21.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    
+    init(store: FeedStore) {
+        
+    }
+    
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount: Int = 0
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+
+}
