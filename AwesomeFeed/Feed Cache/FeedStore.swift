031e2caa5572df00fac944c6ea822bb7625e578a
Tan Tran <tantan39>
2021-06-01 15:20:52 -0400

    Move FeedStore to its own file

---
+++ AwesomeFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  AwesomeFeed
+//
+//  Created by Tan Tan on 6/1/21.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
