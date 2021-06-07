174658a872a309ea702282179ea3a36a8493c5c5
Tan Tran <tantan39>
2021-06-07 15:52:33 -0400

    Extract CoreData managed model classes into separate files

---
+++ AwesomeFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -0,0 +1,31 @@
+//
+//  ManagedCache.swift
+//  AwesomeFeed
+//
+//  Created by Tan Tan on 6/7/21.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+internal class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+    
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+        request.returnsObjectsAsFaults = false
+
+        let cache = try context.fetch(request).first
+        return cache
+    }
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try ManagedCache.find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+    
+    var localFeed: [LocalFeedImage] {
+        return feed.compactMap({ ($0 as? ManagedFeedImage)?.local })
+    }
+}
---
+++ AwesomeFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -0,0 +1,33 @@
+//
+//  ManagedFeedImage.swift
+//  AwesomeFeed
+//
+//  Created by Tan Tan on 6/7/21.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+    
+    static func images(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: feed.map({ local in
+            let manageFeedImage = ManagedFeedImage(context: context)
+            manageFeedImage.id = local.id
+            manageFeedImage.imageDescription = local.description
+            manageFeedImage.location = local.location
+            manageFeedImage.url = local.url
+            return manageFeedImage
+        }))
+    }
+    
+    var local: LocalFeedImage {
+        return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+    }
+    
+}
