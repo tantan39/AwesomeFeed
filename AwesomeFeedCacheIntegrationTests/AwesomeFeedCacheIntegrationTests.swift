948e282e01b1cf975dde68be30c17e56ca50c7bc
Tan Tran <tantan39>
2021-06-07 16:44:00 -0400

    Add `AwesomeFeedCacheIntegrationTests` target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ AwesomeFeedCacheIntegrationTests/AwesomeFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  AwesomeFeedCacheIntegrationTests.swift
+//  AwesomeFeedCacheIntegrationTests
+//
+//  Created by Tan Tan on 6/7/21.
+//
+
+import XCTest
+
+class AwesomeFeedCacheIntegrationTests: XCTestCase {
+
+
+
+}
