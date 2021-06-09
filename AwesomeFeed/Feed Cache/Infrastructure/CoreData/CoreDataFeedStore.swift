c0d9cab82158bff62160e4f496954e729951aa04
Tan Tran <tantan39>
2021-06-09 12:40:13 -0400

    Simplify `CoreDataFeedStore` completion code with the new Result APIs

--- AwesomeFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ AwesomeFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -18,34 +18,25 @@ public class CoreDataFeedStore: FeedStore {
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         perform { context in
-            do {
-                if let cache = try ManagedCache.find(in: context) {
-
-                    completion(.success(.some(CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp))))
-                    
-                } else {
-                    completion(.success(.none))
+            completion( Result(catching: {
+                try ManagedCache.find(in: context).map {
+                    return CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
                 }
-            } catch {
-                completion(.failure(error))
-            }
+            }))
         }
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         perform { context in
-            do {
+            completion( Result(catching: {
                 let manageCache = try ManagedCache.newUniqueInstance(in: context)
                 manageCache.timestamp = timestamp
 
                 manageCache.feed = ManagedFeedImage.images(from: feed, in: context)
                 
                 try context.save()
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
-            
+            }))
+           
         }
         
     }
@@ -53,12 +44,9 @@ public class CoreDataFeedStore: FeedStore {
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
         perform { context in
-            do {
+            completion( Result(catching: {
                 try ManagedCache.find(in: context).map(context.delete).map(context.save)
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+            }))
         }
         
     }
