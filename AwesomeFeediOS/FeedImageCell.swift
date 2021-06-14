0b5c2212b2aa3e8d48ce4672f07d85b3e1655bd9
Tan Tran <tantan39>
2021-06-14 11:05:09 -0400

    Render loaded feed on successful load completion

---
+++ AwesomeFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  AwesomeFeediOS
+//
+//  Created by Tan Tan on 6/14/21.
+//
+
+import UIKit
+
+public class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
--- AwesomeFeediOS/FeedViewController.swift
+++ AwesomeFeediOS/FeedViewController.swift
@@ -10,6 +10,7 @@ import AwesomeFeed
 
 final public class FeedViewController: UITableViewController {
     private var loader: FeedLoader?
+    private var tableModel: [FeedImage] = []
     
     public convenience init(loader: FeedLoader) {
         self.init()
@@ -28,8 +29,23 @@ final public class FeedViewController: UITableViewController {
     
     @objc func load() {
         refreshControl?.beginRefreshing()
-        loader?.load(completion: { [weak self] _ in
+        loader?.load(completion: { [weak self] result in
             self?.refreshControl?.endRefreshing()
+            self?.tableModel = (try? result.get()) ?? []
+            self?.tableView.reloadData()
         })
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        return cell
     }
 }
