2f839c152f53569217133cac8767e3f455bfad1c
Tan Tran <tantan39>
2021-06-14 17:43:28 -0400

    Extract LoaderSpy to a separate file

---
+++ AwesomeFeediOSTests/Controllers/Helpers/FeedViewControllerTests+LoaderSpy.swift
@@ -0,0 +1,72 @@
+//
+//  FeedViewControllerTests+LoaderSpy.swift
+//  AwesomeFeediOSTests
+//
+//  Created by Tan Tan on 6/14/21.
+//
+
+import Foundation
+import AwesomeFeed
+import AwesomeFeediOS
+
+extension FeedViewControllerTests {
+    class LoaderSpy: FeedLoader, FeedImageDataLoader {
+        
+        // MARK: - FeedLoader
+        
+        private var feedRequests = [(FeedLoader.Result) -> Void]()
+        
+        var loadFeedCallCount: Int {
+            return feedRequests.count
+        }
+        
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            feedRequests.append(completion)
+        }
+        
+        func completeFeedLoading(with feed: [FeedImage] = [],at index: Int = 0) {
+            feedRequests[index](.success(feed))
+        }
+        
+        func completeFeedLoadingWithError(at index: Int) {
+            let error = NSError(domain: "an error", code: 0)
+            feedRequests[index](.failure(error))
+        }
+        
+        // MARK: - FeedImageDataLoader
+        
+        private struct TaskSpy: FeedImageDataLoaderTask {
+            let cancelCallback: () -> Void
+            
+            func cancel() {
+                cancelCallback()
+            }
+        }
+        
+        private(set) var imageRequest = [(url: URL,completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedImageURLs: [URL] {
+            imageRequest.map { $0.url }
+        }
+        
+        private(set) var cancelledImageURLs: [URL] = [URL]()
+        
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            imageRequest.append((url, completion))
+            
+            return TaskSpy { [weak self] in
+                self?.cancelledImageURLs.append(url)
+            }
+        }
+        
+        func completeImageLoading(with imageData: Data = Data(),at index: Int = 0) {
+            imageRequest[index].completion(.success(imageData))
+        }
+        
+        func completeImageLoadingWithError(at index: Int = 0) {
+            let error = NSError(domain: "an error", code: 0)
+            imageRequest[index].completion(.failure(error))
+        }
+        
+    }
+}
