24430260592a792912d7815ebebad0773fba1511
Tan Tran <tantan39>
2021-06-24 12:06:38 -0400

    Set `FeedViewController` title

--- AwesomeFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ AwesomeFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -23,7 +23,7 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = "My Feed"
         refresh()
     }
     
