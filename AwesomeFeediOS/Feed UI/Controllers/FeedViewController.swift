f51d4e8ae3b3212cea290f036aa78c2dc19cd680
Tan Tran <tantan39>
2021-06-24 21:29:25 -0400

    Dispatch background feed completion to main thread before updating the UI since UIKit is not thread self

--- AwesomeFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ AwesomeFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -17,7 +17,13 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     
     var tableModel = [FeedImageCellController]() {
         didSet {
-            tableView.reloadData()
+            if Thread.isMainThread {
+                tableView.reloadData()
+            } else {
+                DispatchQueue.main.async { [weak self] in
+                    self?.tableView.reloadData()
+                }
+            }
         }
     }
     
@@ -31,6 +37,7 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     func display(_  viewModel: FeedLoadingViewModel) {
+        guard Thread.isMainThread else { return DispatchQueue.main.async { [weak self] in self?.display(viewModel) }}
         if viewModel.isLoading {
             self.refreshControl?.beginRefreshing()
         } else {
