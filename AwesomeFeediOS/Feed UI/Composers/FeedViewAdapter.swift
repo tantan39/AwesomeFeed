98aa93f461fd7d900a2a2c56f6eb5eb8a68017d3
Tan Tran <tantan39>
2021-06-24 22:16:12 -0400

    Move `FeedViewAdapter` to separate file

---
+++ AwesomeFeediOS/Feed UI/Composers/FeedViewAdapter.swift
@@ -0,0 +1,32 @@
+//
+//  FeedViewAdapter.swift
+//  AwesomeFeediOS
+//
+//  Created by Tan Tan on 6/24/21.
+//
+
+import UIKit
+import AwesomeFeed
+
+final class FeedViewAdapter: FeedView {
+    private weak var controller: FeedViewController?
+    private let imageLoader: FeedImageDataLoader
+    
+    init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+    
+    func display(_ viewModel: FeedViewModel) {
+        self.controller?.tableModel = viewModel.feed.map { model in
+            let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
+
+            let view = FeedImageCellController(delegate: adapter)
+            
+            adapter.presenter = FeedImagePresenter(view: WeakRefVirtualProxy(view), imageTransformer: UIImage.init)
+            
+            return view
+        }
+    }
+    
+}
