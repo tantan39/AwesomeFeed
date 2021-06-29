17291e14e0e50f1b300bc779d101fd22097d851d
Tan Tran <tantan39>
2021-06-29 11:52:52 -0400

    Move `FeedLoadingViewModel` to separate file

---
+++ AwesomeFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  AwesomeFeed
+//
+//  Created by Tan Tan on 6/29/21.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public var isLoading: Bool
+}
