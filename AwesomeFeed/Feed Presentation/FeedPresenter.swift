75d294ff2b194a6cb9030d71d2e262dcc84670f8
Tan Tran <tantan39>
2021-06-29 11:52:03 -0400

    Move `FeedViewModel` to separate file

--- AwesomeFeed/Feed Presentation/FeedPresenter.swift
+++ AwesomeFeed/Feed Presentation/FeedPresenter.swift
@@ -7,10 +7,6 @@
 
 import Foundation
 
-public struct FeedViewModel {
-    public var feed: [FeedImage]
-}
-
 public protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
---
+++ AwesomeFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  AwesomeFeed
+//
+//  Created by Tan Tan on 6/29/21.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public var feed: [FeedImage]
+}
