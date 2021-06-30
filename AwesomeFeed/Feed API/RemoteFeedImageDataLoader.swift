7415cdb7aba365c33638f3c35cf51a168bf7c409
Tan Tran <tantan39>
2021-06-30 17:13:01 -0400

    Move `RemoteFeedImageDataLoader` to production

---
+++ AwesomeFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -0,0 +1,64 @@
+//
+//  RemoteFeedImageDataLoader.swift
+//  AwesomeFeed
+//
+//  Created by Tan Tan on 6/30/21.
+//
+
+import Foundation
+
+public final class RemoteFeedImageDataLoader {
+    private var client: HTTPClient
+    
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
+        private var completion: ((FeedImageDataLoader.Result) -> Void)?
+
+        var wrapped: HTTPClientTask?
+        
+        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
+        }
+        
+        func cancel() {
+            preventFurtherCompletions()
+            wrapped?.cancel()
+        }
+        
+        private func preventFurtherCompletions() {
+            completion = nil
+        }
+    }
+    
+    @discardableResult
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = HTTPClientTaskWrapper(completion)
+        task.wrapped = self.client.get(url: url, completion: { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case let .success((data, response)):
+                if response.statusCode == 200, !data.isEmpty {
+                    task.complete(with: .success(data))
+                 } else {
+                    task.complete(with: .failure(Error.invalidData))
+                 }
+                
+            case let .failure(error):
+                task.complete(with: .failure(error))
+            }
+        })
+        
+        return task
+    }
+}
