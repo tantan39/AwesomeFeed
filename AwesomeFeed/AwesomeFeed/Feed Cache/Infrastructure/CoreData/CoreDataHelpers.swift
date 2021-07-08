b4e7ac685bef5852a6556dc5dcb9bc48c47b8c0e
Tan Tran <tantan39>
2021-07-08 17:26:08 -0400

    Revert "`Load NSManagedObjectModel` instance lazily and cache it to prevent multiple `NSEntityDescriptions` claiming the same `NSManagedObject` model subclasses (This problem just generates warnings but could lead to undefined behavior in the future)."
    This reverts commit 3275cfd3cfe4d3f288e93d9bff4c175920b53e6d.

--- AwesomeFeed/AwesomeFeed/Feed Cache/Infrastructure/CoreData/CoreDataHelpers.swift
+++ AwesomeFeed/AwesomeFeed/Feed Cache/Infrastructure/CoreData/CoreDataHelpers.swift
@@ -8,21 +8,27 @@
 import CoreData
 
 internal extension NSPersistentContainer {
+    enum LoadingError: Swift.Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Swift.Error)
+    }
     
-    static func load(name: String, model: NSManagedObjectModel, url: URL) throws -> NSPersistentContainer {
+    static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else { throw LoadingError.modelNotFound }
+        
         let description = NSPersistentStoreDescription(url: url)
         let container = NSPersistentContainer(name: name, managedObjectModel: model)
         container.persistentStoreDescriptions = [description]
         
         var loadError: Swift.Error?
         container.loadPersistentStores(completionHandler: { loadError = $1 })
-        try loadError.map { throw $0 }
+        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
         
         return container
     }
 }
 
-extension NSManagedObjectModel {
+private extension NSManagedObjectModel {
     static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
         return bundle.url(forResource: name, withExtension: "momd")
             .flatMap({ NSManagedObjectModel(contentsOf: $0) })
