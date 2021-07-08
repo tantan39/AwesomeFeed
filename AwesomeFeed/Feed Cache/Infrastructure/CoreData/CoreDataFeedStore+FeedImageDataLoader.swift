b2f169414a1b04f45091c8ba61ef498c0f38a002
Tan Tran <tantan39>
2021-07-08 10:05:40 -0400

    Move `CoreDataFeedStore` implementation of the `FeedImageDataStore` protocol to production

---
+++ AwesomeFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -0,0 +1,19 @@
+//
+//  CoreDataFeedStore+FeedImageDataLoader.swift
+//  AwesomeFeed
+//
+//  Created by Tan Tan on 7/8/21.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        
+    }
+    
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+    
+}
