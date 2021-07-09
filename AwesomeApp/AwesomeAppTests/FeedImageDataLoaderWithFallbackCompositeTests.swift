0a8e361d47e263ab5ad2ed849c32d961feebe694
Tan Tran <tantan39>
2021-07-09 11:56:08 -0400

    `FeedImageDataLoaderWithFallbackComposite.loadImageData` loads from primary loader first

--- AwesomeApp/AwesomeAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ AwesomeApp/AwesomeAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -22,7 +22,8 @@ class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
     }
     
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        return Task()
+        let task = self.primary.loadImageData(from: url, completion: completion)
+        return task
     }
 }
 
@@ -34,6 +35,24 @@ class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         
         XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
+    
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        _ = sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
+    
+    // MARK: - Helpers
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://a-url.com")!
     }
     
     private class LoaderSpy: FeedImageDataLoader {
