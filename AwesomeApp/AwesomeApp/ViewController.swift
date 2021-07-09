2af565da6304b6a7f3b3a42f0f4e129b39c26ef5
Tan Tran <tantan39>
2021-07-08 20:19:37 -0400

    Add empty `AwesomeApp` project

---
+++ AwesomeApp/AwesomeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AwesomeApp
+//
+//  Created by Tan Tan on 7/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
