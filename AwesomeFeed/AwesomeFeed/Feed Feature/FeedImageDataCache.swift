713ab59feb7569b51b3f6f27bf844f5ba2704928
Tan Tran <tantan39>
2021-07-12 15:16:56 -0400

    Move `FeedImageDataCache` to the `Feed Feature` production group

---
+++ AwesomeFeed/AwesomeFeed/Feed Feature/FeedImageDataCache.swift
@@ -0,0 +1,13 @@
+//
+//  FeedImageDataCache.swift
+//  AwesomeFeed
+//
+//  Created by Tan Tan on 7/12/21.
+//
+
+
+public protocol FeedImageDataCache {
+    typealias Result = Swift.Result<Void, Error>
+    
+    func save(_ data: Data, for url: URL, completion: @escaping (Result) -> Void)
+}
