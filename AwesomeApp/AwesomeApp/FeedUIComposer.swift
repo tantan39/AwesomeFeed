c026902e5b9f7cfcdb2b660dafe096aee5ae1dd4
Tan Tran <tantan39>
2021-08-05 10:25:30 -0400

    Move `dispatchOnMainQueue` operator to the point of use (before subscription with `sink`)

--- AwesomeApp/AwesomeApp/FeedUIComposer.swift
+++ AwesomeApp/AwesomeApp/FeedUIComposer.swift
@@ -13,14 +13,16 @@ import AwesomeFeediOS
 public final class FeedUIComposer {
     private init() {}
     
-    public static func feedComposedWith(feedLoader: @escaping () -> FeedLoader.Publisher, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> FeedViewController {
-        let presentationAdapter = FeedLoaderPresentationAdapter(
-            feedLoader: { feedLoader().dispatchOnMainQueue() })
+    public static func feedComposedWith(
+        feedLoader: @escaping () -> FeedLoader.Publisher,
+        imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> FeedViewController {
+        
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
         
         let feedController = makeWith(delegate: presentationAdapter, title: FeedPresenter.title)
         
         presentationAdapter.presenter = FeedPresenter(feedView: FeedViewAdapter(controller: feedController,
-                                                                                imageLoader: { imageLoader($0).dispatchOnMainQueue() }),
+                                                                                imageLoader: imageLoader),
                                                       loadingView: WeakRefVirtualProxy(feedController),
                                                       errorView: WeakRefVirtualProxy(feedController))
         return feedController
