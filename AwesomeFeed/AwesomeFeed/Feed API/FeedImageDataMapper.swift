7ffd42c0da5b18efa75f25ddd6e9e65975771b31
Tan Tran <tantan39>
2021-08-08 14:25:17 -0400

    Test FeedImageDataMapper in isolation

---
+++ AwesomeFeed/AwesomeFeed/Feed API/FeedImageDataMapper.swift
@@ -0,0 +1,22 @@
+//
+//  FeedImageDataMapper.swift
+//  AwesomeFeed
+//
+//  Created by Tan Tan on 8/8/21.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+            throw Error.invalidData
+        }
+        
+        return data
+    }
+}
