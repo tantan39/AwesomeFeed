811066186c991bd077dbddac953d2b3f43b61925
Tan Tran <tantan39>
2021-08-12 10:48:53 -0400

    Add ImageComments title

---
+++ AwesomeFeed/AwesomeFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentsPresenter.swift
+//  AwesomeFeed
+//
+//  Created by Tan Tan on 8/12/21.
+//
+
+import Foundation
+
+public final class ImageCommentsPresenter {
+    public static var title: String {
+        return NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE", tableName: "ImageComments", bundle: Bundle(for: Self.self), comment: "Title for the image comments view")
+    }
+}
