bcdfa7d0ad90483addca5055cc58f263e284e255
Tan Tran <tantan39>
2021-08-12 12:01:21 -0400

    Rename the typealias

--- AwesomeApp/AwesomeApp/FeedUIComposer.swift
+++ AwesomeApp/AwesomeApp/FeedUIComposer.swift
@@ -13,13 +13,13 @@ import AwesomeFeediOS
 public final class FeedUIComposer {
     private init() {}
     
-    public typealias FeedPresenterAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>
+    public typealias FeedPresentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>
     
     public static func feedComposedWith(
         feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>,
         imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> FeedViewController {
         
-        let presentationAdapter = FeedPresenterAdapter(loader: feedLoader)
+        let presentationAdapter = FeedPresentationAdapter(loader: feedLoader)
         
         let feedController = makeFeedViewController(delegate: presentationAdapter, title: FeedPresenter.title)
         
