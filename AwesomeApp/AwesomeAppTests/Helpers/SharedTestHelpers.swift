a1f312392784f97ae0fc741fd4dd8053cea14ffb
Tan Tran <tantan39>
2021-08-16 11:59:23 -0400

    Move localization helpers to shared scope

--- AwesomeApp/AwesomeAppTests/Helpers/SharedTestHelpers.swift
+++ AwesomeApp/AwesomeAppTests/Helpers/SharedTestHelpers.swift
@@ -24,3 +24,18 @@ func uniqueFeed() -> [FeedImage] {
     return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
 }
 
+private class DummyView: ResourceView {
+    func display(_ viewModel: Any) { }
+}
+
+var loadError: String {
+    return LoadResourcePresenter<Any, DummyView>.loadError
+}
+
+var feedTitle: String {
+    return FeedPresenter.title
+}
+
+var commentsTitle: String {
+    return ImageCommentsPresenter.title
+}
