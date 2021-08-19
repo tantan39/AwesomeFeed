629142973155436e206e67b05d7bbb0c160d7229
Tan Tran <tantan39>
2021-08-19 14:39:00 -0400

    Prevent load more action while loading more

--- AwesomeApp/AwesomeApp/CombineHelpers.swift
+++ AwesomeApp/AwesomeApp/CombineHelpers.swift
@@ -9,6 +9,17 @@ import Foundation
 import AwesomeFeed
 import Combine
 
+public extension Paginated {
+    var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
+        guard let loadMore = loadMore else { return nil }
+        return {
+            Deferred {
+                Future(loadMore)
+            }.eraseToAnyPublisher()
+        }
+    }
+}
+
 public extension HTTPClient {
     typealias Publisher = AnyPublisher<(Data, HTTPURLResponse), Error>
     
