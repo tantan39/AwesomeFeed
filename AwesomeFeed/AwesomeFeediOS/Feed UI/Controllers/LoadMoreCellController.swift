dd90f437b1b617107d509364328f6f29ab96d6f8
Tan Tran <tantan39>
2021-08-19 11:28:25 -0400

    Add "Load more" message label

--- AwesomeFeed/AwesomeFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
+++ AwesomeFeed/AwesomeFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -19,8 +19,12 @@ public class LoadMoreCellController: NSObject, UITableViewDataSource {
     }
 }
 
-extension LoadMoreCellController: ResourceLoadingView {
+extension LoadMoreCellController: ResourceLoadingView, ResourceErrorView {
     public func display(_ viewModel: ResourceLoadingViewModel) {
         cell.isLoading = viewModel.isLoading
+    }
+    
+    public func display(_ viewModel: ResourceErrorViewModel) {
+        cell.message = viewModel.message
     }
 }
