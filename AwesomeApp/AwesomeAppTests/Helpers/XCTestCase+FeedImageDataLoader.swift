53faa960ca78494930723e48b44a124b5fad130e
Tan Tran <tantan39>
2021-08-24 13:15:23 -0400

    Make FeedImageDataLoader sync

--- AwesomeApp/AwesomeAppTests/Helpers/XCTestCase+FeedImageDataLoader.swift
+++ AwesomeApp/AwesomeAppTests/Helpers/XCTestCase+FeedImageDataLoader.swift
@@ -11,10 +11,10 @@ import AwesomeFeed
 protocol FeedImageDataLoaderTestCase: XCTestCase { }
 
 extension FeedImageDataLoaderTestCase {
-    func expect(_ sut: FeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
+    func expect(_ sut: FeedImageDataLoader, toCompleteWith expectedResult: Result<Data, Error>, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         
-        _ = sut.loadImageData(from: anyURL()) { receivedResult in
+        action()
+        let receivedResult = Result { try sut.loadImageData(from: anyURL()) }
             switch (receivedResult, expectedResult) {
             case let (.success(receivedFeed), .success(expectedFeed)):
                 XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
@@ -24,13 +24,6 @@ extension FeedImageDataLoaderTestCase {
                 
             default:
                 XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
         }
-        
-        action()
-        
-        wait(for: [exp], timeout: 1.0)
     }
 }
--- AwesomeFeed/AwesomeFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ AwesomeFeed/AwesomeFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -8,27 +8,6 @@
 import Foundation
 
 public final class LocalFeedImageDataLoader {
-    private final class Task: FeedImageDataLoaderTask {
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        
-        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-        
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            preventFurtherCompletions()
-        }
-        
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-        
-    }
-    
     private let store: FeedImageDataStore
     
     public init(store: FeedImageDataStore) {
@@ -53,21 +32,20 @@ extension LocalFeedImageDataLoader: FeedImageDataCache {
 }
 
 extension LocalFeedImageDataLoader: FeedImageDataLoader {
-    public typealias LoadResult = FeedImageDataLoader.Result
-
     public enum LoadError: Swift.Error {
         case failed
         case notFound
     }
     
-    public func loadImageData(from url: URL, completion: @escaping (LoadResult) -> Void) -> FeedImageDataLoaderTask {
-        let task = Task(completion)
-        task.complete (with: Swift.Result {
-            try store.retrieve(dataForURL: url)
+    public func loadImageData(from url: URL) throws -> Data {
+        do {
+            if let imageData = try store.retrieve(dataForURL: url) {
+                return imageData
+            }
+        } catch {
+            throw LoadError.failed
         }
-        .mapError { _ in LoadError.failed }
-        .flatMap({ data in data.map { .success($0) } ?? .failure(LoadError.notFound) }))
         
-        return task
+        throw LoadError.notFound
     }
 }
